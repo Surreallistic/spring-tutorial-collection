54d046c43a04136c6ac5fb9edc897262da03648d
Surreallistic <iluzion5@o2.pl>
2016-11-30 21:55:59 +0100

    maven exercise

---
+++ Build-project-with-maven/src/main/java/Greeter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Surreallistic on 30.11.2016.
+ */
+public class Greeter {
+    public String getHello() {
+        return "Hello World using Maven.";
+    }
+}
---
+++ Build-project-with-maven/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Surreallistic on 30.11.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.getHello());
+    }
+}
